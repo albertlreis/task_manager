99539c76aff498a22bcfbbc007419ff80af5cd96
Albert Lima dos Reis <albertlreis@gmail.com>
2023-09-23 04:17:10 -0300

    Configuração de cors

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { config } from 'dotenv';
 import authRoutes from './routes/authRoutes.mjs';
 import projectRoutes from './routes/projectRoutes.mjs';
@@ -7,6 +8,15 @@ import taskRoutes from './routes/taskRoutes.mjs';
 config(); // Isso carregará automaticamente as variáveis de ambiente do arquivo .env
 
 const app = express();
+
+// Configurar o CORS para permitir solicitações do seu aplicativo React
+app.use(
+    cors({
+        origin: 'http://localhost:3000', // Substitua pelo URL do seu aplicativo em produção, se necessário
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true, // Habilitar credenciais (cookies, cabeçalhos de autenticação, etc.), se necessário
+    })
+);
 
 // Middleware de análise de corpo JSON
 app.use(express.json());
